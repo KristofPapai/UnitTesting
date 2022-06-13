127da0911fd9ea2cf96b6ec0753bb83930a67f1d
Kristóf Levente Pápai <83229957+KristofPapai@users.noreply.github.com>
2022-06-13 16:49:09 +0200

    small update

--- UnitTestingStudy/UnitTesting/AlmaPiac.cs
+++ UnitTestingStudy/UnitTesting/AlmaPiac.cs
@@ -8,6 +8,10 @@ namespace UnitTesting
 {
     public class AlmaPiac
     {
+        //public double GetAlmaÁr(double kg)
+        //1 paramétere van, hogy hány kilót veszünk, és 1 dolgot ad vissza: az alma árát.Egyetlen csavar van benne,
+        //hogy minél több almát veszünk annál több kedvezményt kapunk.
+        //Legalább 5 kg alma vásárlása esetén a kedvezmény 10 %, 20kg, vagy a felett 15 %. 1 kg alma ára 400Ft.
         static double AlmaArKg = 400.0;
         public double GetAlmaAr(double kg) {
             if (kg >= 5 && kg < 20)
